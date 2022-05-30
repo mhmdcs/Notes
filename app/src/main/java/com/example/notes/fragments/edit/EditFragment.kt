4ea15dc34452d09753e0fed86c79467cbc780a96
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-05-30 10:34:42 +0300

    design menu for fragment_edit layout

--- app/src/main/java/com/example/notes/fragments/edit/EditFragment.kt
+++ app/src/main/java/com/example/notes/fragments/edit/EditFragment.kt
@@ -1,10 +1,8 @@
 package com.example.notes.fragments.edit
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.notes.R
 
 
@@ -14,8 +12,13 @@ class EditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_edit_menu, menu)
+    }
 }
