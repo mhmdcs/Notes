3777cd23d76aaebf2f36ede063b8407749a13ccd
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-06-03 17:15:20 +0300

    create ViewModel

---
+++ app/src/main/java/com/example/notes/data/viewmodel/NotesViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.notes.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.notes.data.NotesDatabase
+import com.example.notes.data.models.NotesData
+import com.example.notes.data.repository.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NotesViewModel(application: Application): AndroidViewModel(application) {
+
+    private val notesDao = NotesDatabase.getDatabase(application).NotesDao()
+
+    private val repository: NotesRepository
+
+    private val getAllData: LiveData<List<NotesData>>
+
+    init {
+        repository = NotesRepository(notesDao)
+
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(notesData: NotesData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(notesData)
+        }
+    }
+
+}
